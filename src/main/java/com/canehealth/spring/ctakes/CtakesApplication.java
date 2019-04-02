532010c6b63c4c2848387e0278bb9ff175cf50ed
Sana <sanariaz154@gmail.com>
2019-04-02 23:28:50 +0500

    main file added

---
+++ src/main/java/com/canehealth/spring/ctakes/CtakesApplication.java
@@ -0,0 +1,19 @@
+package com.canehealth.spring.ctakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class CtakesApplication {
+    static {
+        System.setProperty("ctakes_umlsuser", "whoosis");
+        System.setProperty("ctakes_umlspw", "Enter password here");
+    }
+	public static void main(String[] args) {
+
+        SpringApplication.run(CtakesApplication.class, args);
+	}
+}
